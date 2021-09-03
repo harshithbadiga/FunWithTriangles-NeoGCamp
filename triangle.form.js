7292d768dd8cfe7f66bf8888b287e40017b44deb
harshithbadiga <harshithbadiga@gmail.com>
2021-09-03 23:36:18 +0300

    basic-quiz

---
+++ triangle.form.js
@@ -0,0 +1,25 @@
+var quizForm = document.querySelector("#quiz-questions")
+var checkBtn = document.querySelector("#check-score-btn")
+var outputDiv = document.querySelector(".output")
+
+const correctAnswer = ["90", "right angled"]
+
+
+function checkAnswers() {
+    var score = 0;
+    var index = 0;
+    const formResults = new FormData(quizForm);
+    for (let value of formResults.values()) {
+        if (value === correctAnswer[index]) {
+            score++
+        }
+        score--
+        index++
+    }
+    outputDiv.innerText = "Your total score is " + score
+
+}
+
+
+
+checkBtn.addEventListener("click", checkAnswers)
