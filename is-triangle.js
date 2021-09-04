6753024ad6308a5bd192ce0471f322092c973db0
harshithbadiga <harshithbadiga@gmail.com>
2021-09-04 15:54:57 +0300

    nav added and style

---
+++ is-triangle.js
@@ -0,0 +1,27 @@
+var userAngles = document.querySelectorAll(".angles")
+var checkAnswer = document.querySelector(".checkme")
+var output = document.querySelector(".output")
+
+function triangleOrNot() {
+    checkSum(Number(userAngles[0].value), Number(userAngles[1].value), Number(userAngles[2].value))
+}
+
+function checkSum(angle1, angle2, angle3) {
+    if (angle1 > 0 && angle2 && angle3 > 0) {
+        sum = angle1 + angle2 + angle3
+        if (sum === 180) {
+            output.innerText = "A traingle can be formed!"
+        } else {
+            output.innerText = " A traingle cannot be formed!"
+        }
+    } else {
+        output.innerText = "Enter a value greater than 0 !"
+    }
+
+
+}
+
+
+
+
+checkAnswer.addEventListener("click", triangleOrNot)
